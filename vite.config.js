263960fa455918733d5b09765207ccd3ee363ef9
LLlkoJlHuk <overkot12@gmail.com>
2025-03-14 20:26:37 +0700

    Add htaccess file

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import VitePluginStaticCopy from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginStaticCopy({
+      targets: [
+        {
+          src: '.htaccess',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+});
