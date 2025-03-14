fb3dbfff0b3894bd5e31d2d114c314a7308ec37f
LLlkoJlHuk <overkot12@gmail.com>
2025-03-14 20:23:13 +0700

    Add page's animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { useRoutes } from "react-router-dom";
+import { useRoutes, useLocation  } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import routes from "./routes/index.jsx";
 import SettingsContext from "./context/settings.js";
 import Canvas from "@/canvas/index.jsx";
 import Menu from "@/components/Menu/index.jsx";
 
-
-function App() {
+function AnimatedRoutes() {
+    const location = useLocation();
     const page = useRoutes(routes);
 
     const [settings, setSettings] = useState(() => {
@@ -22,11 +23,27 @@ function App() {
         });
     }
 
-    return <SettingsContext.Provider value={{settings, changeLanguage}}>
-        <Canvas />
-        <Menu />
-        { page ?? <div>Page not found</div> }
-    </SettingsContext.Provider>
+    return (
+        <SettingsContext.Provider value={{settings, changeLanguage}}>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={location.pathname}
+                    initial={{ x: "100%", opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    exit={{ x: "-100%", opacity: 0 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                    <Canvas />
+                    <Menu />
+                    { page ?? <div>Page not found</div> }
+                </motion.div>
+            </AnimatePresence>
+        </SettingsContext.Provider>
+    );
 }
 
-export default App;
+function App() {
+    return <AnimatedRoutes />;
+}
+
+export default App
