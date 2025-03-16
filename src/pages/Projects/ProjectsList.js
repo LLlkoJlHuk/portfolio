d9fbb53d95047df6450dcd08f0a666c587d7d2e8
LLlkoJlHuk <overkot12@gmail.com>
2025-03-16 20:58:54 +0700

    Edit css files for mobile version

--- src/pages/Projects/ProjectsList.js
+++ src/pages/Projects/ProjectsList.js
@@ -3,23 +3,23 @@ import ProjectImg from "../../assets/images/project.jpg"
 const ProjectsList = [
     {
         id: 0,
-        name: 'Vivanti EU',
-        link: 'https://vivanti.eu',
-        preview: ProjectImg,
-        description: {
-            ru: 'Диджитал-агентство, предлагающее услуги на стыке фармацевтического маркетинга, медицины и цифровых технологий',
-            en: 'A digital agency offering services at the intersection of pharmaceutical marketing, medicine and digital technologies'
-        }
-    },
-
-    {
-        id: 1,
         name: 'Vivanti RU',
         link: 'https://vivanti.ru',
         preview: ProjectImg,
         description: {
             ru: 'Диджитал-агентство, предоставляющее цифровой маркетинг и креативные решения для компаний сферы здравоохранения',
             en: 'A digital agency providing digital marketing and creative solutions for healthcare companies'
+        }
+    },
+
+    {
+        id: 1,
+        name: 'Vivanti EU',
+        link: 'https://vivanti.eu',
+        preview: ProjectImg,
+        description: {
+            ru: 'Диджитал-агентство, предлагающее услуги на стыке фармацевтического маркетинга, медицины и цифровых технологий',
+            en: 'A digital agency offering services at the intersection of pharmaceutical marketing, medicine and digital technologies'
         }
     },
 
